0f465f58807cfaef62bc5a1dec07bc4d85c30dfd
Carla Mulligan <S00186667@mail.itsligo.ie>
2020-03-25 12:54:26 +0000

    Add project files.

---
+++ DataManagement/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LabSheet8; //reference to use team data
+
+
+namespace DataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //create new teamdata 
+            TeamData db = new TeamData(); 
+
+            //writing to files it means if anything goes wrong will safely close
+            //all props
+            using(db)
+            {
+                Team t1 = new Team() { TeamID = 1, TeamName = "Sligo Rovers", Location = "Sligo" };
+                Player p1 = new Player() { PlayerID = 1, Name = "Tom", Position = "Forward", TeamID = 1, Team = t1 };
+                Player p2 = new Player() { PlayerID = 2, Name = "Nick", Position = "Defender", TeamID = 1, Team = t1 };
+
+                Team t2 = new Team() { TeamID = 2, TeamName = "Finn Harpes", Location = "Donega;" };
+                Player p3 = new Player() { PlayerID = 3, Name = "Same", Position = "Midfielder", TeamID = 2, Team = t2};
+                Player p4 = new Player() { PlayerID = 4, Name = "Jim", Position = "Goalkeeper", TeamID = 2, Team = t2 };
+
+
+                db.Teams.Add(t1);
+
+                db.Teams.Add(t2);
+
+                Console.WriteLine("Added Teams to database");
+
+                db.Players.Add(p1);
+                db.Players.Add(p2);
+                db.Players.Add(p3);
+                db.Players.Add(p4);
+
+                Console.WriteLine("Added players to database");
+                db.SaveChanges();
+
+                Console.WriteLine("Saved to database");
+
+            }
+
+
+        }
+    }
+}
---
+++ LabSheet8/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSheet8
+{
+    public class Player
+    {
+
+        //this is what is inside the tables
+        public int PlayerID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Position { get; set; }
+
+
+        public int TeamID { get; set; }
+        public virtual Team Team { get; set; }
+
+
+    }
+
+    public class Team
+    {
+
+        //this is what is inside the table 
+        public int TeamID { get; set; }
+
+        public string TeamName { get; set; }
+
+        public string Location { get; set; }
+
+        public virtual List<Player> Players { get; set; }
+    }
+
+
+    public class TeamData : DbContext
+    {
+        //overiding the dbcontext constructor //passing in a name for the context
+        public TeamData() : base("MyTeamData") { }
+
+        //player table
+        public DbSet<Player> Players { get; set; }
+
+        //team table
+        public DbSet<Team> Teams { get; set; }
+
+
+
+    }
+}
